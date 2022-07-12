468840dc603bde046ace4a527f3df08d9b56dd64
ksy133900 <tkdgns13578@gmail.com>
2022-07-12 17:01:16 +0900

    코드업 2일차 문제풀이

--- TIL/codeup/6025.py
+++ TIL/codeup/6025.py
@@ -1,9 +1,8 @@
 #정수 2개를 입력받아
 #합을 출력하는 프로그램을 작성해보자.
 
-x=input()
-y=input()
-x=float(x)
-y=float(y)
+x,y=input().split()
+x=int(x)
+y=int(y)
 hap=x+y
 print(hap)
---
+++ TIL/codeup/6036.py
@@ -0,0 +1,3 @@
+#입력된 단어를 입력된 횟수만큼 반복해 출력한다.
+w,n=input().split()
+print(w*int(n))
