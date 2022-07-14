da4b54c73200c7af2d5e6a593c7316733387a3a3
ksy133900 <tkdgns13578@gmail.com>
2022-07-14 16:44:16 +0900

    코드업 4일차 문제풀이

---
+++ TIL/codeup/6050~6098/6066.py
@@ -0,0 +1,15 @@
+#3개의 정수(a, b, c)가 공백을 두고 입력된다.
+#입력된 순서대로 짝(even)/홀(odd)을 줄을 바꿔 출력한다.
+a,b,c = map(int,input().split())
+if a%2==0:
+    print('even')
+else:
+    print('odd')
+if b%2==0:
+    print('even')
+else:
+    print('odd')
+if c%2==0:
+    print('even')
+else:
+    print('odd')
