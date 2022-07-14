f4070d11b6154d462e5a61745e24a3695c3512aa
ksy133900 <tkdgns13578@gmail.com>
2022-07-14 13:31:14 +0900

    배운 내용 정리

---
+++ TIL/7월/0712/Q.Solve/Q7.py
@@ -0,0 +1,6 @@
+numbers=[0,20,100]
+max=9999
+for i in numbers:
+    if max > i:
+        max = i
+print(max)
---
+++ TIL/7월/0713/3일차 예제/문제 13. 문자열 조작하기 .py
@@ -0,0 +1,11 @@
+#주어진 word를 역순으로 출력해보자
+word = 'apple'
+word2 = ''
+for i in word:
+    word2=i+word2
+print(word2)
+
+word2 = ''
+for i in word:
+    word2 = i+ word2
+print(word2)
