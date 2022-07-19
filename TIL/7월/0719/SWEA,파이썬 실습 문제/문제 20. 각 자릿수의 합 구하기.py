a3dd31f35383efbb5f24a6dff789141ab0110015
ksy133900 <tkdgns13578@gmail.com>
2022-07-20 00:09:53 +0900

    파이썬 06. 실습 문제

--- TIL/7월/0719/SWEA,파이썬 실습 문제/문제 20. 각 자릿수의 합 구하기.py
+++ TIL/7월/0719/SWEA,파이썬 실습 문제/문제 20. 각 자릿수의 합 구하기.py
@@ -7,6 +7,17 @@ c=((number%100)%10)
 sum=a+b+c
 print(sum)
 
+#다른 방법
+number=123
+result=0
+while number:
+    result+=number%10
+    number//=10
+print(result)
+    #! result에 number의 나머지 값을 더하면서 number의 자릿수를 하나씩 줄이고, number가 0이되면 반복 종료
+    #! result 값 1+2+3 = 6이 출력된다.
+
+
 #str()
 number=123
 result=0
