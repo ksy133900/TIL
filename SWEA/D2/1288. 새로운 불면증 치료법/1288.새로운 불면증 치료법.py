f2e2ecce92a31cd06479c085b644924c531af04b
ksy133900 <tkdgns13578@gmail.com>
2022-07-23 23:33:12 +0900

    SWEA D2 문제 풀이

--- SWEA/D2/1288. 새로운 불면증 치료법/1288.새로운 불면증 치료법.py
+++ SWEA/D2/1288. 새로운 불면증 치료법/1288.새로운 불면증 치료법.py
@@ -19,18 +19,7 @@ for a in range(0,p):
         if not data.count(0) :
             print('#%d %d' % (a+1,int(n)))
             break
+        
         n = str(value + int(n))
 
-# 또 다른 방법
-T = int(input())
-for test_case in range(1,T+1):
-    N = int(input())
-    N1 = N
-    numbers = set()  #set에 계속 추가 예정
-    while True:
-        for n in str(N):
-            numbers.add(n)  #number set에 추가
-        if len(numbers) == 10:
-            break
-        N += N1
-    print('#%d %d'% (test_case,N))
+
