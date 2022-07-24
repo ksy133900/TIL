4007eda8af7aa9323c6367797f86f61bb36fd59f
ksy133900 <tkdgns13578@gmail.com>
2022-07-24 17:19:55 +0900

    파이썬 02. 프로젝트

--- TIL/Project/0722/1.인기 영화 조회.py
+++ TIL/Project/0722/1.인기 영화 조회.py
@@ -6,17 +6,15 @@ def vote_average_movies():
     # 0. 평점 8 이상인 영화 목록을 담는 리스트 초기화
     vote_average_movies_over_8 = []
 
-    # 1. `tmdb.py`패키지 내의 `UrlMaker()` 클래스의 `get_url()`메서드를 통해 `url`주소를 받아온다.
     BASE_URL = 'https://api.themoviedb.org/3'
     path = '/movie/popular'
-    # 2. 받아온 `url`로 `requests` 모듈을 활용해, 데이터를 요청하고 응답값을 받아온다.
     params = {
         'api_key': 'ea7605a7a7e59759ce6acfc9527a9e1a',
         'language': 'ko-KR'
 }
-
-    # 3. 응답받아온 데이터를 dictionary로 형변환한다. (`movie_dict`에 할당)
+ 
     response = requests.get(BASE_URL+path, params=params).json()
+    # 3. response의 'results'항목을 `movie_details`에 할당)
     movie_details = response.get('results',None)
 
     # 5. movie_details 반복
