c2b75ba14b480daf8d6f19dd2b757c1226de9dcd
ksy133900 <tkdgns13578@gmail.com>
2022-08-01 17:47:55 +0900

    .

--- TIL/backjoon/백준 문제풀이/1일차/10871 X보다 작은 수.py
+++
@@ -1,6 +0,0 @@
-#https://www.acmicpc.net/problem/10871
-N,X = map(int,input().split())
-A=list(map(int,input().split()))
-for i in A:
-    if i < X :
-        print(i,end=' ')
--- TIL/backjoon/백준 문제풀이/1일차/2742 기찍N.py
+++
@@ -1,4 +0,0 @@
-N=int(input())
-result=0
-for i in reversed(range(N)):
-    print(i+1)
