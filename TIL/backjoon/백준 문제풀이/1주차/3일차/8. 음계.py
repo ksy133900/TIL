09c683e1c7de56ab5e6e12183899a4ff7b3ab234
ksy133900 <tkdgns13578@gmail.com>
2022-08-07 18:50:49 +0900

    알고리즘 1주 백준 문제풀이 3일차

---
+++ TIL/backjoon/백준 문제풀이/1주차/3일차/8. 음계.py
@@ -0,0 +1,12 @@
+import sys
+sys.stdin = open("4. 음계.txt", "r")
+
+numbers = list(map(int,input().split()))
+
+if numbers == [1,2,3,4,5,6,7,8]:
+    print('ascending')
+elif numbers == [8, 7, 6, 5, 4, 3, 2, 1,]:
+    print('descending')
+else:
+    print('mixed')
+
