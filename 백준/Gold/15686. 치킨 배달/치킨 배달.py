361c8563456f9c6151f0dd15617b2eb70397794c
ksy133900 <103582315+ksy133900@users.noreply.github.com>
2022-09-23 17:59:18 +0900

    [Gold V] Title: 치킨 배달, Time: 788 ms, Memory: 30840 KB -BaekjoonHub

---
+++ 백준/Gold/15686. 치킨 배달/치킨 배달.py
@@ -0,0 +1,28 @@
+import sys
+from itertools import combinations
+
+input = sys.stdin.readline
+
+n, m = map(int, input().split())
+city = list(list(map(int, input().split())) for _ in range(n))
+result = 999999
+house = []    
+chick = []     
+
+for i in range(n):
+    for j in range(n):
+        if city[i][j] == 1:
+            house.append([i, j])
+        elif city[i][j] == 2:
+            chick.append([i, j])
+
+for chi in combinations(chick, m): 
+    temp = 0          
+    for h in house: 
+        chi_len = 999   
+        for j in range(m):
+            chi_len = min(chi_len, abs(h[0] - chi[j][0]) + abs(h[1] - chi[j][1]))
+        temp += chi_len
+    result = min(result, temp)
+
+print(result)
