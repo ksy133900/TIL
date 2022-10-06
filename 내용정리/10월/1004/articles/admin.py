edd75caba5a569c87977695ae352b21e03c0cc4b
ksy133900 <tkdgns13578@gmail.com>
2022-10-06 20:54:11 +0900

    1004 정리

--- 내용정리/10월/1004/articles/admin.py
+++ 내용정리/10월/1004/articles/admin.py
@@ -1,10 +1,12 @@
+from tkinter.messagebox import QUESTION
 from django.contrib import admin
 from .models import Article
 
 # Register your models here.
-# https://docs.djangoproject.com/en/3.2/intro/tutorial07/
+
+
 class ArticleAdmin(admin.ModelAdmin):
-    list_display = ("title", "created_at", "updated_at")
+    fields = ["title"]
 
 
 admin.site.register(Article, ArticleAdmin)
