0dc7cdff41334bf98dd8a0350f6e2b1a0cab1ce8
ksy133900 <tkdgns13578@gmail.com>
2022-10-07 16:28:10 +0900

    1007 장고 페어프로그래밍

---
+++ django/1007(페어 프로젝트)/review/models.py
@@ -0,0 +1,14 @@
+from django.db import models
+
+# from django.forms import DateTimeField#
+
+# Create your models here.
+
+
+class Review(models.Model):
+    title = models.CharField(max_length=30)
+    content = models.TextField()
+    movie_name = models.CharField(max_length=30)
+    grade = models.FloatField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
