c1f785a37a7961e1c4ad29a3f98081c56c1f46bb
ksy133900 <tkdgns13578@gmail.com>
2022-10-20 17:58:49 +0900

    1019 django prt

--- django/1019/accounts/views.py
+++ django/1019/accounts/views.py
@@ -1,6 +1,3 @@
-from django.shortcuts import render
-
-# Create your views here.
 from django.shortcuts import render, redirect
 
 # from .models import User
--- django/1019/articles/views.py
+++ django/1019/articles/views.py
@@ -1,4 +1,3 @@
-from xml.etree.ElementTree import Comment
 from django.shortcuts import render, redirect
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
